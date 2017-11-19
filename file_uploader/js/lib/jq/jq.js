66e7fc3c7774033a7a01924f52bc69fc1f4afeb8
takagotch <dyaccb@gmail.com>
2017-11-20 01:52:40 +0900

    file_uploader/js/lib/jq/jq.js

---
+++ file_uploader/js/lib/jq/jq.js
@@ -0,0 +1,9 @@
+#CDN
+#jq
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js">
+</script>
+#jq-UI
+<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.8.6/jquery-ui.min.js">
+</script>
+
+
